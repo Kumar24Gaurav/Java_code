da775608207affae310b81e0bd4f3b495983e4ea
Kumar24Gaurav <kumargaurav24kg@gmail.com>
2024-05-20 15:53:15 +0530

    Learning git

---
+++ src/Lec_3/FibonacciSeriesbysir.java
@@ -0,0 +1,16 @@
+package Lec_3;
+
+public class FibonacciSeriesbysir {
+    public static void main(String[] args) {
+        int n=6;
+        int prev=0;
+        int curr=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(prev+" ");
+            int c=curr+prev;
+            prev=curr;
+            curr=c;
+        }
+        System.out.println(prev);
+    }
+}
