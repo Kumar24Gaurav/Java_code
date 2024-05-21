96b818ca49cb606d0b14aa9fe197ec16c74d45a6
Kumar24Gaurav <kumargaurav24kg@gmail.com>
2024-05-21 14:59:20 +0530

    Learning git

---
+++ src/Lec_5/Swap.java
@@ -0,0 +1,15 @@
+package Lec_5;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a=1;
+        System.out.println("a: " + a);
+        int b=2;
+        System.out.println("b: " + b);
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("current a: " +a);
+        System.out.println("current b: " +b);
+    }
+}
