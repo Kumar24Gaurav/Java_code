63b9262dc2acd4015d00731d80c037e7666ef7dd
Kumar24Gaurav <kumargaurav24kg@gmail.com>
2024-05-21 14:50:15 +0530

    Learning git

---
+++ src/Lec_5/arr2.java
@@ -0,0 +1,11 @@
+package Lec_5;
+
+public class arr2 {
+    public static void main(String[] args) {
+        int a[]=new int[10];
+        for(int i=0;i<a.length;i++){
+            a[i]=i;
+        }
+        System.out.println(a[8]);
+    }
+}
